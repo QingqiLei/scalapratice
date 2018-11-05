ecd1b3f068a7d6279139511a54c2ada92f3faef5
chauncey <qingqi_lei@icloud.com>
2018-11-05 19:35:36 +0800

    class

--- src/scala/objectoriented/Person.scala
+++ src/scala/objectoriented/Person.scala
@@ -1,27 +1,27 @@
 package scala.objectoriented
 
-// 默认public
+// public is default
 class Person {
-  // 属性， 不可变性，相当于只有get方法，var 有get 和set
-val id :String = "100"
+  // 属性， immutable，相当于只有get方法，  var 有get 和set
+  val id: String = "100" // immutable
 
-  var name: String =_
+  var name: String = _ // variable name == null
 
-  private var age:Int = _
-  private[this] val gender = "男"  // 只有本类可以访问
+  private var age: Int = _
+  private[this] val gender = "男" // 只有本类可以访问
 
 }
 
-object Person{ // 伴生对象
+object Person { // 伴生对象
   def main(args: Array[String]): Unit = {
     val p = new Person()
-
-    println(p.id )
+    println(p.id)
     p.age = 19
-    println(p.age )
+    println(p.age)
+
   }
 }
 
-object Test1{
+object Test1 {
 
 }
